554586b91644a55aac8be2568bdd842f110837a1
Dörte de Kok <me@doerte.eu>
2023-02-24 13:43:49 +0100

    working on test, commented out testing in pyproject.toml

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -2,4 +2,13 @@
 """
 #import pytest
 
-##tests need to be implemented
+import pandas as pd
+from discuit_package.my_module import prepare_data
+
+d = {'keep': {0: 1.0, 1: 2.0, 2: 3.0}, 'drop': {0: 1.0, 1: 2.0, 2: 3.0}}
+df = pd.DataFrame(d)
+
+d_out = {"keep": {0: 1.0, 1: 2.0, 2: 3.0}}
+
+def check_data():
+    assert prepare_data([df],[],[],[],['drop']) == d_out
