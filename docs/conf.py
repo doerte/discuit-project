e889a12dca3ced16bd65f719f599487701fc7654
Dörte de Kok <me@doerte.eu>
2023-02-26 19:08:21 +0100

    added intersphinx mapping and coverage again

--- docs/conf.py
+++ docs/conf.py
@@ -80,9 +80,9 @@ html_theme = "sphinx_rtd_theme"
 
 intersphinx_mapping = {'python': ('https://docs.python.org/3', None),
                        # Commonly used libraries, uncomment when used in package
-                       # 'numpy': ('http://docs.scipy.org/doc/numpy/', None),
-                       # 'scipy': ('http://docs.scipy.org/doc/scipy/reference/', None),
-                       # 'scikit-learn': ('https://scikit-learn.org/stable/', None),
+                        'numpy': ('http://docs.scipy.org/doc/numpy/', None),
+                        'scipy': ('http://docs.scipy.org/doc/scipy/reference/', None),
+                        'scikit-learn': ('https://scikit-learn.org/stable/', None),
                        # 'matplotlib': ('https://matplotlib.org/stable/', None),
-                       # 'pandas': ('http://pandas.pydata.org/docs/', None),
+                        'pandas': ('http://pandas.pydata.org/docs/', None),
                        }
